b413bf14135adcf3749dffce1bac5497ccbd392c
Mathias Rav <rav@cs.au.dk>
2017-05-14 19:57:03 -0400

    Make highlight pattern part of config

--- handlers/highlight.py
+++ handlers/highlight.py
@@ -3,8 +3,12 @@ from handlers.hostnotify import create_and_show_notification
 
 
 class Handler:
+    async def load(self, client):
+        self.client = client
+        self.pattern = client.config.HIGHLIGHT
+
     async def handle_pubmsg(self, connection, event):
-        if re.search(r'\bmort(able)?\b', event.args, re.I):
+        if re.search(self.pattern, event.args):
             create_and_show_notification(
                 'From %s in %s' % (event.source.split('!')[0], event.target),
                 event.args)
