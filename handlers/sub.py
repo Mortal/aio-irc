d111aa35adf9d7c3e2e7d263b33ea3d45f3ea428
Mathias Rav <rav@cs.au.dk>
2017-05-22 22:33:02 -0400

    Try to handle ratelimit messages

--- handlers/sub.py
+++ handlers/sub.py
@@ -1,4 +1,7 @@
+import time
 import random
+import asyncio
+import traceback
 
 
 MSGS = (
@@ -23,6 +26,11 @@ try:
 except NameError:
     last_msg = None
 
+try:
+    repost_mutex
+except NameError:
+    repost_mutex = asyncio.Lock()
+
 
 def get_next_msg():
     global last_msg
@@ -34,6 +42,9 @@ def get_next_msg():
 
 
 class Handler:
+    def __init__(self):
+        self.last_msg = {}
+
     def should_post(self, system_msg, target):
         if not system_msg:
             print("No system-msg")
@@ -51,6 +62,7 @@ class Handler:
 
     async def post(self, connection, target):
         msg = get_next_msg()
+        self.last_msg[target] = (msg, time.time())
         print("Send message to %s: %r" % (target, msg))
         await connection.privmsg(target, msg)
 
@@ -72,3 +84,34 @@ class Handler:
         if not self.should_post(system_msg, event.target):
             return
         await self.post(connection, event.target)
+
+    async def handle_pubnotice(self, connection, event):
+        tags = {
+            k: v
+            for kv in (event.tags or ())
+            for k, v in [(kv['key'], kv['value'])]
+        }
+        if tags.get('msg-id') == 'msg_ratelimit':
+            self.msg_ratelimit(connection, event.target)
+
+    def msg_ratelimit(self, connection, target):
+        loop = connection.loop
+        try:
+            last_msg, last_msg_time = self.last_msg[target]
+        except KeyError:
+            return
+        elapsed = time.time() - last_msg_time
+        if elapsed > 1:
+            return
+        loop.create_task(self.repost(connection, last_msg))
+
+    async def repost(self, connection, target, msg):
+        try:
+            await repost_mutex.acquire()
+            await asyncio.sleep(1)
+            print("Repost message to %s: %r" % (target, msg))
+            await connection.privmsg(target, msg)
+        except Exception:
+            traceback.print_exc()
+        finally:
+            repost_mutex.release()
