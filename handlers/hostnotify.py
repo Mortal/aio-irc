1c043d401161cefa8d89d81583a1acce07a569c1
Mathias Rav <rav@cs.au.dk>
2017-05-27 13:32:01 -0400

    More robust hostnotify handling

--- handlers/hostnotify.py
+++ handlers/hostnotify.py
@@ -1,3 +1,4 @@
+import asyncio
 import webbrowser
 from aiotwirc.timer import spamming
 
@@ -30,6 +31,7 @@ def create_and_show_notification(summary, message, icon=None, key=None):
 class Handler:
     async def load(self, client):
         self.client = client
+        self.host_mode = {}
 
     async def handle_hosttarget(self, connection, event):
         hosting_channel = event.target.lstrip('#')
@@ -38,12 +40,29 @@ class Handler:
         if target_channel == '-':
             summary = f'{hosting_channel}: Exit host mode'
             message = f'{hosting_channel} is no longer in host mode.'
+            self.host_mode[hosting_channel] = 'host_off'
         else:
             summary = f'{hosting_channel}: Hosting {target_channel}'
             message = f'{hosting_channel} is now hosting {target_channel}.'
+            self.host_mode[hosting_channel] = 'host_on'
         create_and_show_notification(summary, message, key='hosttarget')
-        if target_channel == '-' and not spamming(60):
+        if target_channel == '-':
+            self.client.loop.create_task(self.delayed_open(hosting_channel))
+
+    async def delayed_open(self, hosting_channel):
+        # Wait for 1 second and check if the host mode is still 'host_off'.
+        await asyncio.sleep(1)
+        # If it's 'host_on' or 'host_target_went_offline', don't do anything.
+        if self.host_mode[hosting_channel] == 'host_off' and not spamming(60):
+            # Probably the stream actually started -- open webbrowser.
             webbrowser.open('https://twitch.tv/' + hosting_channel)
-        if hosting_channel == 'darbian' and target_channel == '-':
-            self.client.set_default_msg(
-                'darbBro hi chat, hi darb, good luck on the runs tonight!')
+            if hosting_channel == 'darbian':
+                self.client.set_default_msg(
+                    'darbBro hi chat, hi darb, good luck on the runs tonight!')
+
+    async def handle_pubnotice(self, connection, event):
+        hosting_channel = event.target.lstrip('#')
+        msg_id = next((o['value'] for o in event.tags if o['key'] == 'msg-id'),
+                      None)
+        if msg_id in ('host_on', 'host_off', 'host_target_went_offline'):
+            self.host_mode[hosting_channel] = msg_id
